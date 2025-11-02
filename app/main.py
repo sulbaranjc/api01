6079b165723d7272e673208c7d335167197cb2a4
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2025-11-02 22:21:12 +0100

    Eliminar línea en blanco y ajustar formato en el archivo main.py

--- app/main.py
+++ app/main.py
@@ -19,8 +19,8 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
 # para ejecutar la aplicación, use el siguiente comando:
 # uvicorn app.main:app --reload
 # o simplemente:
